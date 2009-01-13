48a2105e9dfe99a4d030f9fcaa81d6e7da77f4e7
Leon Brocard <acme@astray.com>
2009-01-13 09:09:04 +0000

    And release 0.49

--- lib/Net/Amazon/S3.pm
+++ lib/Net/Amazon/S3.pm
@@ -140,7 +140,7 @@ has 'ua'     => ( is => 'rw', isa => 'LWP::UserAgent', required => 0 );
 has 'err'    => ( is => 'rw', isa => 'Maybe[Str]',     required => 0 );
 has 'errstr' => ( is => 'rw', isa => 'Maybe[Str]',     required => 0 );
 
-our $VERSION = '0.48';
+our $VERSION = '0.49';
 
 my $KEEP_ALIVE_CACHESIZE = 10;
 

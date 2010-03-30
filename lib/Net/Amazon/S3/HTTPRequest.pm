6c0d303bc567c0b37a2a53dd95554d0eeac8014f
Leon Brocard <acme@astray.com>
2010-03-30 10:18:35 +0100

    fix authenticated urls to work with EU buckets (patch by Edmund von der Burg)

--- lib/Net/Amazon/S3/HTTPRequest.pm
+++ lib/Net/Amazon/S3/HTTPRequest.pm
@@ -67,7 +67,11 @@ sub query_string_authentication_uri {
         = $self->_encode( $aws_secret_access_key, $canonical_string );
 
     my $protocol = $self->s3->secure ? 'https' : 'http';
-    my $uri = URI->new("$protocol://s3.amazonaws.com/$path");
+    my $uri = "$protocol://s3.amazonaws.com/$path";
+    if ( $path =~ m{^([^/?]+)(.*)} && _is_dns_bucket($1) ) {
+        $uri = "$protocol://$1.s3.amazonaws.com$2";
+    }
+    $uri = URI->new($uri);
 
     $uri->query_param( AWSAccessKeyId => $aws_access_key_id );
     $uri->query_param( Expires        => $expires );
